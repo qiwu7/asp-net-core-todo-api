6d733ba426a9a0c9979a434f418740e054c8d5fe
Qi Wu <qi.wu@elliemae.com>
2018-09-10 11:16:43 -0700

    add controller

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you cannot delete all TodoItems
+                _context.TodoItems.Add(new TodoItem { Name = "item1" });
+                _context.SaveChanges();
+            }
+        }
+    }
+}
